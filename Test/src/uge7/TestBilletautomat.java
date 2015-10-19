12f65b129e02bafbada18e43c539187d5da1bb4f
Rune Hou Thode <runehouthode@gmail.com>
2015-10-19 16:37:52 +0200

    Første to tests

--- Test/src/uge7/TestBilletautomat.java
+++ Test/src/uge7/TestBilletautomat.java
@@ -4,11 +4,36 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import automat.Billetautomat;
+
 public class TestBilletautomat {
 
 	@Test
-	public void testNegativeBeloeb() {
-		
+	public void testIndsætNegativeBeloeb() {
+		Billetautomat automat = new Billetautomat();
+		automat.setBilletpris(-10);
+		assertEquals(0, automat.getBalance());
 	}
 
+	@Test
+	public void testKoebUdenPenge() {
+		Billetautomat automat = new Billetautomat();
+		automat.indsætPenge(8);
+		automat.udskrivBillet();
+		assertEquals(8, automat.getBalance());
+	}
+	
+	@Test
+	public void testNegativeBeloeb() {
+		Billetautomat automat = new Billetautomat();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testNegativeBeloeb() {
+		Billetautomat automat = new Billetautomat();
+		
+		assertEquals(expected, actual);
+	}
 }
